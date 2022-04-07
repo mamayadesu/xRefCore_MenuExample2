5ec4773c2af025ee9c5379925e83e5e2ec8c4830
mamayadesu <mamayadesu@gmail.com>
2022-04-08 01:54:18 +0500

    New item example

--- src/Program/Main.php
+++ src/Program/Main.php
@@ -2,35 +2,42 @@
 
 namespace Program;
 
+use CliForms\MenuBox\MenuBoxTypes;
 use IO\Console;
 use CliForms\MenuBox\MenuBox;
 use CliForms\MenuBox\MenuBoxItem;
-use CliForms\MenuBox\MenuBoxTypes;
-use Data\String\BackgroundColors;
-use Data\String\ForegroundColors;
 
 class Main
 {
     public function __construct(array $args)
     {
         $menu = new MenuBox("oOoOoOo My First Menu oOoOoOo", $this, MenuBoxTypes::KeyPressType);
-        $menu->SetDescription("This is an example of menu. Move between elements using keyboard arrows and press ENTER to selected element.");
+        $menu->SetClearWindowOnRender(true);
+        $menu->SetDescription("This is an example of menu");
+        $menu->SetInputTitle("Input item number and press Enter, honey");
+        $menu->SetWrongItemTitle("Item with same number doesn't exist. Please, try again, dear!");
+        
         $menu->
             AddItem((new MenuBoxItem("Hello world", function(MenuBox $menu)
             {
                 $this->HelloWorld($menu);
             })))->
-            
+
             AddItem((new MenuBoxItem("foo bar", function(MenuBox $menu)
             {
                 $this->foobar($menu);
             })))->
-            
+
             AddItem((new MenuBoxItem("Third item", function(MenuBox $menu)
             {
                 $this->AnyIdeaToMethodName($menu);
             })))->
-            
+
+            AddItem((new MenuBoxItem("Move me to second item", function(MenuBox $menu)
+            {
+                $this->MoveMeToSecondItem($menu);
+            })))->
+
             SetZeroItem((new MenuBoxItem("Close Menu", function(MenuBox $menu)
             {
                 $menu->Close();
@@ -66,5 +73,10 @@ class Main
     public function AnyIdeaToMethodName(MenuBox $menu) : void
     {
         $menu->ResultOutputLine("s a m p l e t e x t");
+    }
+
+    public function MoveMeToSecondItem(MenuBox $menu) : void
+    {
+        $menu->SelectedItemNumber = 2;
     }
 }
